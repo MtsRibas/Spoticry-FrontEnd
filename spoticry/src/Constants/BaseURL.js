bf2c34ef493da8084d33506d7f2b39b354fe7002
Matheus Ribas <matheusribas2013@gmail.com>
2024-11-30 13:55:37 -0300

    Definicao de cores do projeto e baseUrl

---
+++ spoticry/src/Constants/BaseURL.js
@@ -0,0 +1,2 @@
+export const BaseURL =
+  "https://mqjnto3qw2.execute-api.us-east-1.amazonaws.com/default";
---
+++ spoticry/src/Constants/Colors.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  Laranja: "#ff9e00",
+  Preto: "#000000",
+  Branco: " #ffffff",
+  Cinza: "#1b1b1b",
+  Vermelho: "#ff0009",
+  Cinza_Escuro: "#1E1D1D",
+};
