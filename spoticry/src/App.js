d26cbaf71d0b15a3fdc1c038a1c88772b0d861dc
Matheus Ribas <matheusribas2013@gmail.com>
2024-12-01 16:05:31 -0300

    Ajuste de navegacoes

--- spoticry/src/App.js
+++ spoticry/src/App.js
@@ -1,16 +1,10 @@
+// src/App.js
+
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
-import { Home } from "./pages/Home/Home";
-import { LandingPage } from "./pages/LandingPage/LandingPage";
-import { Login } from "./pages/Login/Login";
-import { PlaylistUser } from "./pages/PlaylistsUser/PlaylistUser";
-import { MusicUser } from "./pages/MusicUser/MusicUser";
-import { DetalhePlaylist } from "./pages/PlaylistsUser/DetalhePlaylist";
-import { DetalheMusica } from "./pages/MusicUser/DetalheMusica";
-import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import { BrowserRouter } from "react-router-dom";
+import Rotas from "./Components/Rotas/Rotas";
 import { Colors } from "./Constants/Colors";
-import { Error } from "./pages/Error/Error";
 
 const Page = styled.div`
   padding: 10px 210px;
@@ -29,51 +23,7 @@ function App() {
   return (
     <BrowserRouter>
       <Page>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login Logando={handleLogin} />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute element={<Home />} isLoggedIn={isLoggedIn} />
-            }
-          />
-          <Route
-            path="/playlists"
-            element={
-              <PrivateRoute
-                element={<PlaylistUser />}
-                isLoggedIn={isLoggedIn}
-              />
-            }
-          />
-          <Route
-            path="/playlist/:playlistId"
-            element={
-              <PrivateRoute
-                element={<DetalhePlaylist />}
-                isLoggedIn={isLoggedIn}
-              />
-            }
-          />
-          <Route
-            path="/musics"
-            element={
-              <PrivateRoute element={<MusicUser />} isLoggedIn={isLoggedIn} />
-            }
-          />
-          <Route
-            path="/musica/:id"
-            element={
-              <PrivateRoute
-                element={<DetalheMusica />}
-                isLoggedIn={isLoggedIn}
-              />
-            }
-          />
-
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Rotas isLoggedIn={isLoggedIn} handleLogin={handleLogin} />
       </Page>
     </BrowserRouter>
   );
--- spoticry/src/Components/Rotas/Rotas.js
+++ spoticry/src/Components/Rotas/Rotas.js
@@ -1,23 +1,53 @@
+// src/Routes.js
+
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Home } from "../Home/Home";
-import { Login } from "../Login/Login";
-import { PlaylistUser } from "../PlaylistsUser/PlaylistUser";
-import { MusicUser } from "../MusicUser/MusicUser";
-import { DetalhePlaylist } from "../PlaylistsUser/DetalhePlaylist";
-import { DetalheMusica } from "../MusicUser/DetalheMusica";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../../pages/Home/Home";
+import { LandingPage } from "../../pages/LandingPage/LandingPage";
+import { Login } from "../../pages/Login/Login";
+import { PlaylistUser } from "../../pages/PlaylistsUser/PlaylistUser";
+import { MusicUser } from "../../pages/MusicUser/MusicUser";  
+import { DetalhePlaylist } from "../../pages/PlaylistsUser/DetalhePlaylist";
+import { DetalheMusica } from "../../pages/MusicUser/DetalheMusica";
 import { Error } from "../../pages/Error/Error";
+import PrivateRoute from "../../Components/PrivateRoute/PrivateRoute";
 
-const Router = () => (
-  <Routes>
-    <Route path="/home" element={<Home />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/playlists" element={<PlaylistUser />} />
-    <Route path="/musics" element={<MusicUser />} />
-    <Route path="/playlist/:playlistId" element={<DetalhePlaylist />} />
-    <Route path="/musica/:id" element={<DetalheMusica />} />
-    <Route path="*" element={<Error />} />
-  </Routes>
-);
+const Rotas = ({ isLoggedIn, handleLogin }) => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login Logando={handleLogin} />} />
+      <Route
+        path="/home"
+        element={<PrivateRoute element={<Home />} isLoggedIn={isLoggedIn} />}
+      />
+      <Route
+        path="/playlists"
+        element={
+          <PrivateRoute element={<PlaylistUser />} isLoggedIn={isLoggedIn} />
+        }
+      />
+      <Route
+        path="/playlist/:playlistId"
+        element={
+          <PrivateRoute element={<DetalhePlaylist />} isLoggedIn={isLoggedIn} />
+        }
+      />
+      <Route
+        path="/musics"
+        element={
+          <PrivateRoute element={<MusicUser />} isLoggedIn={isLoggedIn} />
+        }
+      />
+      <Route
+        path="/musica/:id"
+        element={
+          <PrivateRoute element={<DetalheMusica />} isLoggedIn={isLoggedIn} />
+        }
+      />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+};
 
-export default Router;
+export default Rotas;
