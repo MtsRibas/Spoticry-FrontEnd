a9d1daf75889c5d2040d0962e75acabcbf396ed1
Micael Ferrari <micaelferrari211002@gmail.com>
2024-12-01 07:40:22 -0300

    Editmodal e AdicionarMusicaModal

---
+++ spoticry/src/Components/Modais/AdicionarMusicaModal.js
@@ -0,0 +1,67 @@
+import React from "react";
+import detalheModal from "../../assets/Icons/btnIcon.svg";
+import {
+  ModalWrapper,
+  ModalContent,
+  TituloModal,
+  Form,
+  Separacao,
+  Input,
+  BotaoEnviar,
+  BotaoFechar,
+} from "../Style/StyledAdicionarMusicaModal";
+
+export function AdicionarMusicaModal({
+  aberto,
+  fechado,
+  songTitle,
+  setSongTitle,
+  artist,
+  setArtist,
+  url,
+  setUrl,
+  saveSong,
+}) {
+  return (
+    <ModalWrapper aberto={aberto}>
+      <ModalContent>
+        <BotaoFechar onClick={fechado}>X</BotaoFechar>
+        <TituloModal>
+          <img src={detalheModal} alt="Icone músicas" />
+          <h2>Adicionar Música</h2>
+        </TituloModal>
+
+        <Form onSubmit={(e) => e.preventDefault()}>
+          <Separacao>
+            <label>Título</label>
+            <Input
+              type="text"
+              value={songTitle}
+              onChange={(e) => setSongTitle(e.target.value)}
+              placeholder="Título da Música"
+            />
+          </Separacao>
+          <Separacao>
+            <label>Artista</label>
+            <Input
+              type="text"
+              value={artist}
+              onChange={(e) => setArtist(e.target.value)}
+              placeholder="Artista"
+            />
+          </Separacao>
+          <Separacao>
+            <label>URL</label>
+            <Input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder="URL da Música"
+            />
+          </Separacao>
+          <BotaoEnviar onClick={saveSong}>Salvar Música</BotaoEnviar>
+        </Form>
+      </ModalContent>
+    </ModalWrapper>
+  );
+}
---
+++ spoticry/src/Components/Modais/EditModal.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Modal } from "./Modal";
+import { toast } from "react-toastify";
+import { Form, Input, BotaoEnviar } from "../Style/StyledEditModal";
+import { updatePlaylist } from "../Requisicoes/Reqs";
+
+export function EditModal({ playlist, onClose, onUpdate }) {
+  const [novoNome, setNovoNome] = useState(playlist.name || "");
+  const [novaDescricao, setNovaDescricao] = useState(
+    playlist.description || ""
+  );
+
+  const handleUpdate = async () => {
+    if (!novoNome || !novaDescricao) {
+      toast.error("Preencha todos os campos!");
+      return;
+    }
+
+    const token = localStorage.getItem("authenticacao");
+    try {
+      await updatePlaylist(
+        playlist._id,
+        { name: novoNome, description: novaDescricao },
+        token
+      );
+      toast.success("Playlist atualizada com sucesso!");
+      if (onUpdate) onUpdate();
+      onClose();
+    } catch (error) {
+      console.error(
+        "Erro ao atualizar playlist:",
+        error.response || error.message
+      );
+      toast.error("Não foi possível atualizar a playlist.");
+    }
+  };
+
+  return (
+    <Modal
+      aberto={true}
+      fechado={onClose}
+      conteudoModal={
+        <>
+          <h2>Editar Playlist</h2>
+          <Form onSubmit={(e) => e.preventDefault()}>
+            <label>Editar Nome</label>
+            <Input
+              type="text"
+              value={novoNome}
+              onChange={(e) => setNovoNome(e.target.value)}
+              placeholder="Novo Nome da Playlist"
+            />
+            <label>Editar Descricao</label>
+            <Input
+              type="text"
+              value={novaDescricao}
+              onChange={(e) => setNovaDescricao(e.target.value)}
+              placeholder="Nova descricao"
+            />
+            <BotaoEnviar type="button" onClick={handleUpdate}>
+              Salvar Alterações
+            </BotaoEnviar>
+          </Form>
+        </>
+      }
+    />
+  );
+}
